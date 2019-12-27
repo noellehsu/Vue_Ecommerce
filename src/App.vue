e73f9dc7063eaaf3d373f5fa0af03973af0257a2
Noelle Hsu <ohyanana2@gmail.com>
2019-12-27 12:02:04 +0800

    正在製作登入畫面

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    </div> -->
+   
+   <router-view> </router-view>
+
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+
+
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+export default {
+  name:'App',
+  created(){
+    const api = 'https://vue-course-api.hexschool.io/api/noelle/products' //遠端路徑，api前面要加上伺服器路徑
+    this.$http.get(api).then((response) => {
+    console.log(response.data)
+})
   }
 }
+</script>
+
+
+<style lang="scss">
+@import "./assets/all";
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import HelloWorld from '../components/HelloWorld';
+import Login from '../components/pages/Login';
 
 Vue.use(VueRouter)
+
+
+const router = new VueRouter({
+  routes:[
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld,
+    },
+    
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
+    
+  ]
+})
+
+export default router
 
 const routes = [
   {
@@ -17,11 +39,5 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
 ]
-
-const router = new VueRouter({
-  routes
-})
-
-export default router
