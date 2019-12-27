e3e332aa20f7f22c4cf88741d7703d08cc8e00c3
Noelle Hsu <ohyanana2@gmail.com>
2019-12-27 12:05:11 +0800

    index.ts出錯

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import HelloWorld from '../components/HelloWorld';
-import Login from '../components/pages/Login';
+import HelloWorld from '@/components/HelloWorld';
+import Login from '@/components/pages/Login';
 
 Vue.use(VueRouter)
 
