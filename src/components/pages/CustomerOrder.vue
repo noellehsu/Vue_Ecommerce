97c1ce5d82c446e1afbd811862e23aa0a99d8968
Noelle Hsu <ohyanana2@gmail.com>
2020-01-03 08:37:34 +0800

    加上addToCart方法

--- src/components/pages/CustomerOrder.vue
+++ src/components/pages/CustomerOrder.vue
@@ -32,7 +32,11 @@
               <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
               查看更多
             </button>
-            <button type="button" class="btn btn-outline-danger btn-sm ml-auto">
+            <button
+              type="button"
+              class="btn btn-outline-danger btn-sm ml-auto"
+              @click="addToCart(item.id)"
+            >
               <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
               加到購物車
             </button>
@@ -59,7 +63,7 @@
             </button>
           </div>
           <div class="modal-body" style=" align:center">
-            <img :src="singleProduct.imageUrl" style="width:470px" />
+            <img :src="singleProduct.imageUrl" class="container-fluid" style="width:470px" />
             <h6>{{ singleProduct.description }}</h6>
             <br />
             <div class="d-flex justify-content-between align-items-baseline">
@@ -72,25 +76,65 @@
             <br />
 
             <div class="container">
-            <div class="row">
-                <label for="exampleFormControlSelect1" style="width:440px" >選購數量</label>
-                <select class="form-control" id="exampleFormControlSelect1">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
+              <div class="row">
+                <select v-model="singleProduct.num">
+                  <option :value="num" v-for="num in 10" :key="num">選購{{num}} {{singleProduct.unit}}</option>
                 </select>
+              </div>
             </div>
           </div>
-</div>
 
           <div class="modal-footer">
-            <button type="button" class="btn btn-primary">加到購物車</button>
+            <div class="text-muted text-nowrap mr-3">
+              小計
+              <strong>{{singleProduct.num * singleProduct*price}}</strong>
+            </div>
+            <button
+              type="button"
+              class="btn btn-primary"
+              @click="addToCart(singleProduct.id,singleProduct.num)"
+            >
+              <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === singleProduct.id"></i>
+              加到購物車
+            </button>
           </div>
         </div>
       </div>
+    </div></br>
+
+<!-- 購物車 -->
+    <div class="row">
+      <div class="col-2"></div>
+      <div class="col-8">
+          <h5 class="p-3 mb-2 mt-3 d-flex justify-content-center bg-light">購物車</h5>
+    <table class="table table-sm mt-3">
+      <thead>
+        <tr>
+          <th width="30"></th>
+          <th width="100"></th>
+          <th class="text-center">商品名稱</th>
+          <th width="80">數量</th>
+          <th width="80">單價</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in products" :key="item.id">
+          <td class="align-middle text-center"></td>
+          <td>
+            <img src="img/側背包/Tone Oilnume 兩用托特包.jpg" style="width: 150px;" class="img-thumbnail" />
+          </td>
+          <td class="align-middle text-center">Tone Oilnume 兩用托特包</td>
+          <td class="align-middle">1件</td>
+          <td class="align-middle">$10020</td>
+        </tr>
+      
+      </tbody>
+    </table>
+      </div>
+      <div class="col-2"></div>
     </div>
+
+  
   </div>
 </template>
 
@@ -109,8 +153,7 @@ export default {
     };
   },
   methods: {
-    getProducts(page = 1) {
-      //參數預設值為1
+    getProducts() {
       const api = "https://vue-course-api.hexschool.io/api/noelle/products";
       const vm = this;
       vm.isLoading = true;
@@ -130,11 +173,38 @@ export default {
         console.log(response);
         vm.status.loadingItem = "";
       });
+    },
+    addToCart(id, qty = 1) {
+      const api = "https://vue-course-api.hexschool.io/api/noelle/cart";
+      const vm = this;
+      vm.status.loadingItem = id;
+      //定義購物車
+      const cart = {
+        product_id: id,
+        qty
+      };
+
+      this.$http.post(api, { data: cart }).then(response => {
+        console.log(response);
+        vm.status.loadingItem = "";
+        vm.getCart(); //加入購物車後，購物車商品要更新
+        $("#productModal").modal("hide");
+      });
+    },
+    getCart() {
+      const api = "https://vue-course-api.hexschool.io/api/noelle/cart";
+      const vm = this;
+      vm.isLoading = true;
+      this.$http.get(api).then(response => {
+        console.log(response.data);
+        vm.isLoading = false;
+      });
     }
   },
   //一定要有created不然getProducts不會被觸發
   created() {
     this.getProducts(); //created之後才能取得資料，方法要寫在created之後才呼叫
+    this.getCart();
   }
 };
 </script>
